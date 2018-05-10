dd75652095c4d35be6594a0d658a863f3f8ce4c8
Daniel Hutton <dhutton@tensor.co.uk>
2018-05-10 14:39:23 +0100

    Updated smart contract address

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ var env = process.env.NODE_ENV || 'dev';
 if (env === 'dev' || env === 'development') {
   dappAddress = 'n1xGxDjVdMaP5Mk5FpCoEK9gGwpsNgW7BUL';
 } else {
-  dappAddress = 'n1qiDxiZDFRZFG2ezppoP1paniwUz4NGyAB';
+  dappAddress = 'n1rx5yePFaVy9HA1MTcMqPxYy71h1BxYBLw';
+  // TX Hash - 47d3fb7e0c25eddfb6e443496f5a49285cb50894c9f66ba8a3ec00fff61e0c3b
 }
 
 /* GET home page. */
